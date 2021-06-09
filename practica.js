94d08c638492015b21ac09a3fc7f9554b8b803b0
LizaBrUm <elizabeth.bravo@cea.edu.co>
2021-06-09 09:28:37 -0500

    practica

---
+++ practica.js
@@ -0,0 +1,15 @@
+//Mi primera practica en Java Script 
+// -Comentario
+/* Comentario Multilìnea*/
+// Autor: Elizabeth Bravo 
+const nombre = 'Elizabeth Bravo';
+var tipo_doc = '11 05 2005';
+let edad= 16;
+console.log ('Helllo '+nombre+'. Your document is: '+tipo_doc+'yYo are '+edad);
+document.write  // para escribir en el documento
+let anyos = "16";
+let years = parseInt(anyos);
+let comparacion = edad=anyos;
+console.log (comparacion);
+console.log (edad+anyos);
+console.log (years>=edad);
