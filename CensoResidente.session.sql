19dc20f3193b15c72cab3f952fbb37d96b13d25c
LizaBrUm <elizabeth.bravo@cea.edu.co>
2021-09-01 07:35:06 -0500

    here

--- CensoResidente.session.sql
+++ CensoResidente.session.sql
@@ -6,6 +6,16 @@ CREATE TABLE parqueo
         coef_parqueadero FLOAT,
     )
 
+INSERT INTO parqueadero
+(num_parqueadero, matri_parqueadero, coef_parqueadero) 
+VALUES ('D12','234378236','14.64')
+
+SELECT matri_parqueadero FROM parqueadero WHERE coef_parqueadero = '3'
+
+UPDATE parqueadero SET COEF_parqueadero = '23.87' WHERE matri_parqueadero = '14.64'
+
+DELETE FROM parqueadero WHERE num_parqueadero = 'D3'
+
 CREATE TABLE mascota
     (
         id_mascota SERIAL,
@@ -18,10 +28,15 @@ CREATE TABLE mascota
         CONSTRAINT FK_PK_residente FOREIGN KEY (id_residente) REFERENCES residente(id_residente)
     )
 
--- Inserts (CREATE)
 INSERT INTO mascota
-(id_residente, raza_mascota, nombre_mascota, vacuna_mascota, especie, id_residente)
-VALUES ('4567', 'Siames','Romy','No','Gato')
+(id_residente, raza_mascota, nombre_mascota, vacuna_mascota, especie)
+VALUES ('4567', 'Siames','Romy','4254','Gato')
+
+SELECT  nombre_mascota FROM mascota WHERE raza_mascosta = 'Tigrillo'
+
+UPDATE mascota SET vacuna_mascota = '6543' WHERE vacuna_mascota = '4254'
+
+DELETE FROM mascota WHERE id_mascota = '43902'
 
 CREATE TABLE residente
     (
@@ -41,6 +56,15 @@ CREATE TABLE residente
         CONSTRAINT PK_residente PRIMARY KEY (id_residente)
     )
 
+INSERT INTO residente
+(id_)
+
+SELECT
+
+UPDATE
+
+DELETE
+
 CREATE TABLE vehiculo
     (
         id_vehiculo SERIAL,
@@ -48,9 +72,60 @@ CREATE TABLE vehiculo
         placa VARCHAR,
         modelo VARCHAR,
         marca VARCHAR,
-        id_residente VARCHAR,
-
+        id_residente VARCHAR
     )
+
+INSERT INTO vehiculo
+(matr_)
+
+SELECT
+
+UPDATE
+
+DELETE
+
+CREATE TABLE propietario
+    (
+        id_propietario SERIAL,
+        tipo_id_proietario VARCHAR,
+        num_identificacion VARCHAR,
+        tel_propietario BIGINT,
+        ocupacion VARCHAR,
+        edad SMALLINT,
+        nom_propietario VARCHAR,
+        apel_propietario VARCHAR,
+        vacuna BOOLEAN,
+        razon_social VARCHAR,
+        correo_propietario VARCHAR
+    )
+
+INSERT INTO propietario
+(matr_)
+
+SELECT
+
+UPDATE
+
+DELETE
+
+CREATE TABLE apartamento
+    (
+        id_apto SERIAL,
+        matricula_apto VARCHAR,
+        num_apto VARCHAR,
+        bloque VARCHAR,
+        coef_apto FLOAT
+    )
+
+INSERT INTO apartamento
+(matr_)
+
+SELECT
+
+UPDATE
+
+DELETE
+
 --Inserts (Create)
 INSERT INTO parqueadero
 (num_parqueadero, matri_parqueadero, coef_parqueadero) 
@@ -66,40 +141,3 @@ UPDATE parqueadero SET num_parqueadero = 'D9' WHERE num_parqueadero = 'D10'
 
 --Deletes (Delete)
 DELETE FROM parqueadero WHERE id_parqueadero = '1'
-
-INSERT INTO parqueadero
-(num_parqueadero, matri_parqueadero, coef_parqueadero) 
-VALUES ('D12','234378236','14.64')
-
-SELECT matri_parqueadero FROM parqueadero WHERE coef_parqueadero = '3'
-
-UPDATE parqueadero SET matri_parqueadero = 'D11' WHERE matri_parqueadero = 'D12'
-
-DELETE FROM parqueadero WHERE coef_parqueadero = '2'
-
-INSERT INTO mascosta
-(id_)
-
-SELECT  
-
-UPDATE
-
-DELETE
-
-INSERT INTO residente
-(id_)
-
-SELECT
-
-UPDATE
-
-DELETE
-
-INSERT INTO vehiculo
-(matr_)
-
-SELECT
-
-UPDATE
-
-DELETE
